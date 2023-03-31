678d2bf4e2a3126540daa3dc54c8631b7796a191
NaDo2383 <narandorj.byambadorj@gmail.com>
2023-03-31 10:28:56 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_DB_STRING)
+  .then(() => console.log("Database successfully connected"))
+  .catch((err) => console.log(err));
+
+app.get("/api", (req, res) => {
+  res.json({ message: "welcome Rest API" });
+});
+
+app.listen(port, () => console.log("server is running " + process.env.PORT));
