228446b9d45431ebc2eaae90488c3b4422ecb570
NaDo2383 <narandorj.byambadorj@gmail.com>
2023-04-19 11:58:22 +0800

    20230419 nado

--- controllers/properties.contoller.ts
+++ controllers/properties.contoller.ts
@@ -2,31 +2,31 @@ import { Request, Response } from "express";
 import Properties from "../models/properties.model";
 
 const create = async (req: Request, res: Response) => {
-    try {
-        const result = await Properties.create(req.body);
-        res.json({ status: true, result });
-    } catch (err) {
-        res.json({ status: false, message: err });
-    }
+  try {
+    const result = await Properties.create(req.body);
+    res.json({ status: true, result });
+  } catch (err) {
+    res.json({ status: false, message: err });
+  }
 };
 
 const getAll = async (req: Request, res: Response) => {
-    try {
-        const result = await Properties.find().limit(10);
-        res.json({ status: true, result });
-    } catch (err) {
-        res.json({ status: false, message: err });
-    }
+  try {
+    const result = await Properties.find().limit(10);
+    res.json({ status: true, result });
+  } catch (err) {
+    res.json({ status: false, message: err });
+  }
 };
 
 const getOne = async (req: Request, res: Response) => {
-    const { _id } = req.params;
-    try {
-        const result = await Properties.find({ _id });
-        res.json({ status: true, result });
-    } catch (err) {
-        res.json({ status: false, err });
-    }
+  const { _id } = req.params;
+  try {
+    const result = await Properties.findById({ _id });
+    res.json({ status: true, result });
+  } catch (err) {
+    res.json({ status: false, err });
+  }
 };
 
 export { create, getAll, getOne };
