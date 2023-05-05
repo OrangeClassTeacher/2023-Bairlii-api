328203d1e8097679e08b280a152b5f1d0335ce54
NaDo2383 <narandorj.byambadorj@gmail.com>
2023-05-05 14:30:07 +0800

    20230505 nado

--- controllers/propertiesRating.controller.ts
+++ controllers/propertiesRating.controller.ts
@@ -2,31 +2,37 @@ import { Request, Response } from "express";
 import Prorating from "../models/propertiesRating.model";
 
 const create = async (req: Request, res: Response) => {
-  try {
-    const result = await Prorating.create(req.body);
-    res.json({ status: true, result });
-  } catch (err) {
-    res.json({ status: false, message: err });
-  }
+    try {
+        const result = await Prorating.create(req.body);
+        res.json({ status: true, result });
+    } catch (err) {
+        res.json({ status: false, message: err });
+    }
 };
 
 const getAll = async (req: Request, res: Response) => {
-  try {
-    const result = await Prorating.find().limit(10);
-    res.json({ status: true, result });
-  } catch (err) {
-    res.json({ status: false, message: err });
-  }
+    try {
+        const result = await Prorating.find().limit(10);
+        res.json({ status: true, result });
+    } catch (err) {
+        res.json({ status: false, message: err });
+    }
 };
 
 const getOne = async (req: Request, res: Response) => {
-  const { _id } = req.params;
-  try {
-    const result = await Prorating.findById({ _id });
-    res.json({ status: true, result });
-  } catch (err) {
-    res.json({ status: false, err });
-  }
+    const { _id } = req.params;
+    try {
+        const ratingCount = await Prorating.count({ propertyID: _id });
+        const rating = await Prorating.aggregate([
+            { $group: { _id: "$propertyID", avg_val: { $avg: "$rating" } } },
+            { $match: { _id: _id } },
+            { $project: { _id: 1, avg_val: 1 } },
+        ]);
+
+        res.json({ status: true, rating, ratingCount });
+    } catch (err) {
+        res.json({ status: false, err });
+    }
 };
 
 export { create, getAll, getOne };
