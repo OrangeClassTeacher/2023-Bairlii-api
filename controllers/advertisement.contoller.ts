9b9248c4bc62a62e2608c5aa2c35cafa029ffb55
Uuganbayr Bymbajaw <byambajawbebe@Uuganbayrs-MacBook-Pro.local>
2023-05-12 10:59:23 +0800

    2023-0512

--- controllers/advertisement.contoller.ts
+++ controllers/advertisement.contoller.ts
@@ -2,40 +2,40 @@ import { Request, Response } from "express";
 import Advertisements from "../models/advertisement.model";
 
 const create = async (req: Request, res: Response) => {
-    try {
-        const result = await Advertisements.create(req.body);
-        res.json({ status: true, result });
-    } catch (err) {
-        res.json({ status: false, message: err });
-    }
+  try {
+    const result = await Advertisements.create(req.body);
+    res.json({ status: true, result });
+  } catch (err) {
+    res.json({ status: false, message: err });
+  }
 };
 
 const getAll = async (req: Request, res: Response) => {
-    const { pageNumber } = req.body;
+  const { pageNumber } = req.body;
 
-    try {
-        const rowCount = await Advertisements.find().count();
-        const result = await Advertisements.find()
-            .limit(9)
-            .skip(9 * (pageNumber - 1))
-            .populate({ path: "userID" })
-            .populate({ path: "propertyID" });
-        res.json({ status: true, result, rowCount });
-    } catch (err) {
-        res.json({ status: false, message: err });
-    }
+  try {
+    const rowCount = await Advertisements.find().count();
+    const result = await Advertisements.find()
+      .limit(12)
+      .skip(12 * (pageNumber - 1))
+      .populate({ path: "userID" })
+      .populate({ path: "propertyID" });
+    res.json({ status: true, result, rowCount });
+  } catch (err) {
+    res.json({ status: false, message: err });
+  }
 };
 
 const getOne = async (req: Request, res: Response) => {
-    const { _id } = req.params;
-    try {
-        const result = await Advertisements.findById({ _id })
-            .populate({ path: "userID" })
-            .populate({ path: "propertyID" });
-        res.json({ status: true, result });
-    } catch (err) {
-        res.json({ status: false, err });
-    }
+  const { _id } = req.params;
+  try {
+    const result = await Advertisements.findById({ _id })
+      .populate({ path: "userID" })
+      .populate({ path: "propertyID" });
+    res.json({ status: true, result });
+  } catch (err) {
+    res.json({ status: false, err });
+  }
 };
 
 export { create, getAll, getOne };
